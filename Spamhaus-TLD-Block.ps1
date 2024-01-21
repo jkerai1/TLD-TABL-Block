c2bd1c85a343a766cd06700689e799627bbd0460
J/ <55988027+jkerai1@users.noreply.github.com>
2024-01-21 09:23:30 +0000

    Create Spamhaus-TLD-Block.ps1

---
+++ Spamhaus-TLD-Block.ps1
@@ -0,0 +1,11 @@
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline
+$BlockList = Invoke-WebRequest -URI 'https://raw.githubusercontent.com/cyb3rmik3/Hunting-Lists/main/spamhaus-abused-tlds.csv'| Select -expand Content
+
+foreach($line in $BlockList.Split([Environment]::NewLine)){
+    if (-Not $line.StartsWith("TLD")){
+        $TLD = "*" + $line.Substring(0,$line.IndexOf(',')) + "/*"
+        Write-Host($TLD)
+        New-TenantAllowBlockListItems -ListType Url -Block -Entries $TLD -NoExpiration -Notes "Spamhaus commonly abused TLD. List maintained by cyb3rmik3"
+        }
+}
