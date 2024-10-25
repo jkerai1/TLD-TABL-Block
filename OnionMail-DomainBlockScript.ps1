d91bc319ac7c187d2f909d0c48be3793b0431173
J/ <55988027+jkerai1@users.noreply.github.com>
2024-10-25 22:26:32 +0000

    Update OnionMail-DomainBlockScript.ps1

--- OnionMail-DomainBlockScript.ps1
+++ OnionMail-DomainBlockScript.ps1
@@ -2,7 +2,7 @@ Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 $BlockList = Invoke-WebRequest -URI 'https://raw.githubusercontent.com/jkerai1/TLD-TABL-Block/refs/heads/main/OnionMail.txt'| Select -expand Content
 
-foreach($line in $BlockList.Split([Environment]::NewLine)){
+foreach($line in $BlockList.Split("`n")){
     Write-Host($line)
     if (-Not $line.StartsWith("#")){
         New-TenantAllowBlockListItems -ListType Sender -Block -Entries $line -NoExpiration -Notes "OnionMail"
