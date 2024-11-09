4dd6f4bbeb4e2ea680aa57ce0e848bc5feb0a618
J/ <55988027+jkerai1@users.noreply.github.com>
2024-11-09 09:31:34 +0000

    Create TempEmail-BlockSenderDomains.ps1

---
+++ TempEmail-BlockSenderDomains.ps1
@@ -0,0 +1,10 @@
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline
+$BlockList = Invoke-WebRequest -URI 'https://raw.githubusercontent.com/jkerai1/TLD-TABL-Block/refs/heads/main/tempmail-abused%20emaildomains.txt'| Select -expand Content
+
+foreach($line in $BlockList.Split("`n")){
+    Write-Host($line)
+    if (-Not $line.StartsWith("#")){
+        New-TenantAllowBlockListItems -ListType Sender -Block -Entries $line -NoExpiration -Notes "Temporary Email"
+        }
+}
