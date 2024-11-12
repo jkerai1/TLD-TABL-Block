0ad84efb0137a39e811a4b53a440d7ed4ca070b4
J/ <55988027+jkerai1@users.noreply.github.com>
2024-11-12 13:13:43 +0000

    Update InfoSecCA-TLDBlockScript.ps1

--- InfoSecCA-TLDBlockScript.ps1
+++ InfoSecCA-TLDBlockScript.ps1
@@ -2,10 +2,11 @@
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 $BlockList = Invoke-WebRequest -URI 'https://www.info-sec.ca/tld-block.txt'| Select -expand Content
+$exclusion = @('info','example')
 
 foreach($line in $BlockList.Split([Environment]::NewLine)){
-    Write-Host($line)
-    if (-Not $line.StartsWith("#")){
+    if (-Not $line.StartsWith("#") -and ($exclusion -notcontains ($line))){
+        Write-Host($line)
         $TLD = "*." + $line + "/*"
         New-TenantAllowBlockListItems -ListType Url -Block -Entries $TLD -NoExpiration -Notes "Blocked TLD https://www.info-sec.ca/tld-block.txt"
         }
